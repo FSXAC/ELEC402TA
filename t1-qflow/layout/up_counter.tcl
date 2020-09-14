ac6ad425480d88cb52c314e0bb8246b492e01709
Muchen He <i@muchen.ca>
2020-09-13 19:33:02 -0600

    updated tcl

--- t1-qflow/layout/up_counter.tcl
+++ t1-qflow/layout/up_counter.tcl
@@ -4,18 +4,13 @@ l gnd
 h vdd
 
 # make a bus using these nodes
-# vector count_out count\[7\] count\[6\] count\[5\] count\[4\] count\[3\] count\[2\] count\[1\] count\[0\]
-# vector count_out2 count\[3\] count\[2\] count\[1\] count\[0\]
-# vector count_out3 count\[3:0\]
-# vector count_out4 count\[7:0\]
-
-vector count_out count<7:0>
+vector count_out count\<7:0\>
 
 # Start analyzer window
 analyzer
 
 # Add nodes to waveform window
-ana clk rst en count_out count_out2 count_out3 count_out4 
+ana clk rst en count_out
 
 # setvector N 0d220 (this is to set a vector)
 # here is equivalent to initial begin
